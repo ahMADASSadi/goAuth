06164cd11dbad28b8fde4e794bf2fc29e5b64952
ahMADASSadi <ds1380ahmad@email.com>
2025-09-09 16:08:44 +0330

    feat: enhance user retrieval API with optional phone number filtering and update Swagger documentation

--- internal/server/api/user.go
+++ internal/server/api/user.go
@@ -1,8 +1,8 @@
 package api
 
 import (
-	"goAuth/internal/utils/pagination"
 	"goAuth/internal/server/api/schema"
+	"goAuth/internal/utils/pagination"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,7 +11,7 @@ import (
 
 type UserService interface {
 	GetUser(id uint8) *schema.User
-	GetUsers(page, pageSize int, baseURL string) *schema.UserList
+	GetUsers(page, pageSize int, baseURL string, phoneNumber *string) *schema.UserList
 }
 
 type UserHandler struct {
@@ -26,6 +26,18 @@ func NewUserHandler(service UserService) *UserHandler {
 	}
 }
 
+// GetUser godoc
+//
+//	@Summary		Get user by ID
+//	@Description	Retrieves a user by their unique ID.
+//	@Tags			Users
+//	@Accept			json
+//	@Produce		json
+//	@Param			id	path		int	true	"User ID"
+//	@Success		200	{object}	schema.User "returns the user details including the id and phone number"
+//	@Failure		400	{object}	common.ErrorResponse
+//	@Failure		404	{object}	common.ErrorResponse
+//	@Router			/api/v1/users/{id} [get]
 func (h *UserHandler) GetUser(c *fiber.Ctx) error {
 	idStr := c.Params("id")
 	if idStr == "" {
@@ -52,15 +64,31 @@ func (h *UserHandler) GetUser(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(user)
 }
 
+// GetUsers godoc
+//
+//	@Summary		List users
+//	@Description	Retrieves a paginated list of users, with optional phone number search.
+//	@Tags			Users
+//	@Accept			json
+//	@Produce		json
+//	@Param			page			query	int		false	"Page number"				default(1)
+//	@Param			page_size		query	int		false	"Number of users per page"	default(10)
+//	@Param			phone_number	query	string	false	"Filter by phone number"
+//	@Success		200				{array}	schema.User
+//	@Router			/api/v1/users [get]
 func (h *UserHandler) GetUsers(c *fiber.Ctx) error {
+	var phoneNumber *string
 	// Parse pagination parameters from query
 	params := pagination.ParsePaginationFromQuery(c)
+	phoneNumberQuery := c.Query("phone_number")
+	if phoneNumberQuery != "" {
+		phoneNumber = &phoneNumberQuery
+	}
 
-	// Get base URL for pagination links
 	baseURL := pagination.GetBaseURL(c)
 
 	// Get paginated users
-	users := h.service.GetUsers(params.Page, params.PageSize, baseURL)
+	users := h.service.GetUsers(params.Page, params.PageSize, baseURL, phoneNumber)
 	if users == nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"status":  "error",
